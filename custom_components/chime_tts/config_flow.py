9b4f6a9e76bcd206e49e790a380172b7715cf5a5
Nimrod Dolev <nimrod.dolev@gmail.com>
2023-08-14 14:31:01 +0300

    Removed bearer token step from `config_flow`

--- custom_components/chime_tts/config_flow.py
+++ custom_components/chime_tts/config_flow.py
@@ -1,54 +1,16 @@
-"""Adds config flow for TTS Chime."""
-from __future__ import annotations
-
-import voluptuous as vol
+"""Adds config flow for Chime TTS."""
 from homeassistant import config_entries
-from homeassistant.helpers import selector
+from .const import DOMAIN
 
-from homeassistant.const import (
-    HTTP_BEARER_AUTHENTICATION,
-)
-from .const import DOMAIN, NAME, DESCRIPTION, LOGGER
+@config_entries.HANDLERS.register(DOMAIN)
 
-
-class TTSChimeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for TTS Chime."""
+class ChimeTTSFlowHandler(config_entries.ConfigFlow):
+    """Config flow for Chime TTS."""
 
     VERSION = 1
-    _client = None
 
-    data: dict | None = None
-
-    async def async_step_user(
-        self,
-        user_input: dict | None = None,
-    ) -> config_entries.FlowResult:
-        """Request input of long-lived bearer token for REST API."""
-        _errors = {}
-        if user_input is not None:
-            if user_input[HTTP_BEARER_AUTHENTICATION] is not None:
-                self.data = user_input
-                LOGGER.info("tts_chime - config_flow - Adding integration entry")
-                return self.async_create_entry(
-                    title=NAME, data=self.data, description=DESCRIPTION
-                )
-
-            else:
-                LOGGER.warning("No long-lived bearer token entered during setup")
-                _errors["base"] = "empty_token"
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        HTTP_BEARER_AUTHENTICATION, default=""
-                    ): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.TEXT
-                        ),
-                    )
-                }
-            ),
-            errors=_errors,
-        )
+    async def async_step_user(self, user_input=None):
+        """Chime TTS async_step_user."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+        return self.async_create_entry(title="Chime TTS", data={})
