557c8d49c8fb79ce8565add55c30af6a55b6c0ee
Nimrod Dolev <nimrod.dolev@gmail.com>
2024-03-19 18:58:04 +0200

    Customized 404 page

---
+++ src/theme/NotFound/Content/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import Heading from '@theme/Heading';
+
+import './styles.css'
+
+export default function NotFoundContent({ className }) {
+  return (
+    <main className={clsx('container margin-vert--xl', className)}>
+      <div className="row">
+        {/* <div className="col col--6 col--offset-3"> */}
+        <div class="animContainer">
+          <img src="./img/chime_tts.png" class="bottom-image hideAnimation" />
+          <img src="./img/animations/missing_chime_tts.png" class="bottom-image showAnimation hidden" />
+          <div class="top-image animation"></div>
+        </div>
+        {/* </div> */}
+        <div className="col col--6 col--offset-1">
+          <Heading as="h1" className="hero__title">
+            <Translate
+              id="theme.NotFound.title"
+              description="The title of the 404 page">
+              Page Not Found
+            </Translate>
+          </Heading>
+          <p>
+            <Translate
+              id="theme.NotFound.p1"
+              description="The first paragraph of the 404 page">
+              We could not find what you were looking for.
+            </Translate>
+          </p>
+          <p>
+            <Translate
+              id="theme.NotFound.p2"
+              description="The 2nd paragraph of the 404 page">
+              Please contact the owner of the site that linked you to the
+              original URL and let them know their link is broken.
+            </Translate>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
